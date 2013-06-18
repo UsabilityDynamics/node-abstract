9745d81535fa30c58a3bde4529f419a9273d2663
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-06-18 18:00:06 -0400

    Mocha options updated.

--- test/core.js
+++ test/core.js
@@ -6,26 +6,64 @@
  * @type {*}
  */
 
-var Abstract = require( 'abstract' );
+var Abstract = require( '../' );
 
 module.exports = {
-
-  "before": function() {
-    require( 'mocha' );
-    require( 'should' );
-  },
 
   "log": {
 
     "config can be set via method properties.": function() {
-      Abstract.log.config.depth = 2;
-      Abstract.log( 'one' );
+      //Abstract.log.config.depth = 2;
+      //Abstract.log( 'one' );
     },
 
     "is chainable.": function() {
-      Abstract.log( 'one' ).log( 'two' );
+      //Abstract.log( 'one' ).log( 'two' );
     }
 
-  }
+  },
 
+  'basic': function() {
+
+    var Abstraction = new Abstract();
+
+    var User = {
+      name: 'Andy',
+      age: 20,
+      sex: 'male',
+      alive: true,
+      education: {
+        undergraduate: 'campbell university',
+        graduate: 'webster',
+        high_school: 'woodbury high',
+        specialties: {
+          business: [ 'one', 'two', 'three' ],
+          accounting: [ 'advanced', 'quickbooks' ]
+        }
+      },
+      skills: [ 'php', 'jquery' ]
+    }
+
+    var Target = Abstraction({
+      name: {
+        first: Abstraction.get( User, 'name' ),
+        last: 'Potanin'
+      },
+      meta: {
+        sex: Abstraction.get( User, 'sex' ),
+        age: Abstraction.get( User, 'age' ),
+        phone: Abstraction.get( User, 'does.not.exist.phone_number' ),
+        specialties: Abstraction.get( User, 'education.specialties' ),
+
+      },
+      business: Abstraction.get( User, 'education.specialties.business' ),
+      prototype: Abstraction.get( User )
+    });
+
+    Target.meta.specialties.strategy = [ 'planning', 'budgeting' ];
+
+    // console.log( Target );
+    // console.log( User );
+
+  }
 };
