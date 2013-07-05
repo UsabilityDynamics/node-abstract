c8f07f8c2e81453ed813bf2917ffe913320a56b3
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-07-05 12:49:10 -0400

    Added prototype injection functionality and tests.

--- test/core.js
+++ test/core.js
@@ -50,23 +50,69 @@ module.exports = {
           description: 'Used for storing majority of user information.'
         },
         identify: {
-          value: function identify( say ) { return [ say, this._id, this._model, this.channel ]; },
+          value: function identify( say ) { return [ say, this._id, this._type, this._path ]; },
           inheritable: false
         }
       });
 
+      User.should.have.property( '_meta' );
       User.should.have.property( '_id' );
-      User.should.have.property( '_model' );
+      User.should.have.property( '_type' );
+      User.should.have.property( '_path' );
       User.should.have.property( 'identify' );
-      User.should.have.property( 'channel' );
 
       //User.identify( 'prefix' ).toString().should.equal([ 'prefix', 'User', 'Abstract', 'Abstract' ].toString());
 
     },
 
-    "event emitter.": function() {
+    "can create() new Objects and inject multiple prototypes into chain.": function( done ) {
+      var Abstract = module.exports.Abstract;
 
-      var abstract = require( 'abstract' );
+      // Create New Object,
+      var User = Abstract.create( null, require( 'Faker' ).Helpers.createCard() );
+
+      // Add Async and EventEmitter2
+      User.injectPrototype( require( 'async' ) );
+      User.injectPrototype( require( 'eventemitter2' ).EventEmitter2.prototype );
+
+      // Set some EE Options
+      User.listenerTree = {};
+      User.delimiter = ':';
+      User.wildcard = true;
+
+      //User.onAny( function( data ) { console.log( this.event, data ? data : '' ); });
+      //User.on( 'acquire:*', function( data ) { console.log( 'AQUISITION!', this.event ); });
+
+      User.on( 'acquire:sugar', function() {
+        setTimeout( function() { User.emit( 'have:sugar', null, true ); }, 120 );
+      });
+
+      User.on( 'acquire:milk', function() {
+        setTimeout( function() { User.emit( 'have:milk', null, true ); }, 50 );
+      });
+
+      User.on( 'walk:to_car', function() {
+        setTimeout( function() { User.emit( 'in:car', null, true ); }, 100 );
+      });
+
+      User.auto({
+        milk: function( next ) {
+          User.emit( 'acquire:milk' );
+          User.once( 'have:milk', next );
+        },
+        sugar: function( next ) {
+          User.emit( 'acquire:sugar' );
+          User.once( 'have:sugar', next );
+        },
+        verify_groceries: [ 'milk', 'sugar', function( next ) {
+          User.emit( 'walk:to_car' );
+          User.once( 'in:car', next );
+        }],
+        done_shopping: [ 'verify_groceries', function( next ) {
+          User.emit( 'shopping:complete' );
+          done();
+        }]
+      });
 
     }
 
