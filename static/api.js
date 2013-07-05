8ae08e579d7b6760a6cc36bcfdcaeb2222679612
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-07-05 17:55:47 -0400

    Automatic update.

---
+++ static/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Abstract",
+        "ObjectExtender"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
